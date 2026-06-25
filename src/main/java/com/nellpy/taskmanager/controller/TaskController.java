a048a82b61919536a8f73e8400332ebc46723906
Ansgar Nell <ansgar.nell@hiq.de>
2026-06-25 11:55:00 -0300

    main: added TaskRepository and TaskController methods.

--- src/main/java/com/nellpy/taskmanager/controller/TaskController.java
+++ src/main/java/com/nellpy/taskmanager/controller/TaskController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -69,6 +70,18 @@ public class TaskController {
                     taskRepository.delete(task);
                     return ResponseEntity.ok().<Void>build();
                 }).orElse(ResponseEntity.notFound().build());
+    }
+
+
+    @GetMapping("/completed/{status}")
+    public List<Task> getTasksByCompletionStatus(@PathVariable boolean status) {
+        return taskRepository.findByCompleted(status);
+    }
+
+
+    @GetMapping("/search")
+    public List<Task> searchTasksByTitle(@RequestParam String title) {
+        return taskRepository.findByTitleContainingIgnoreCase(title);
     }
 
 }
--- src/main/java/com/nellpy/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/nellpy/taskmanager/repository/TaskRepository.java
@@ -2,10 +2,21 @@ package com.nellpy.taskmanager.repository;
 
 import com.nellpy.taskmanager.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByCompleted(boolean completed);
+
+    List<Task> findByTitleContainingIgnoreCase(String title);
+
+    @Query("SELECT t FROM Task t WHERE t.completed = :completed")
+    List<Task> findTasksByCompletionStatus(@Param("completed") boolean completed);
 
 }
