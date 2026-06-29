00d26c5e0bd09140052cc3a6a54b592476e322ca
Ansgar Nell <ansgar.nell@hiq.de>
2026-06-29 10:38:50 -0300

    main: implemented Exception handling.

--- src/main/java/com/nellpy/taskmanager/controller/TaskController.java
+++ src/main/java/com/nellpy/taskmanager/controller/TaskController.java
@@ -36,10 +36,8 @@ public class TaskController {
 
 
     @GetMapping("/{id}")
-    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
-        return taskService.findById(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    public Task getTaskById(@PathVariable Long id) {
+        return taskService.findById(id);
     }
 
 
@@ -51,17 +49,15 @@ public class TaskController {
 
 
     @PutMapping("/{id}")
-    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
-        return taskService.updateTask(id, updatedTask)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    public Task updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
+        return taskService.updateTask(id, updatedTask);
     }
 
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
-        return taskService.deleteTask(id) ?
-                ResponseEntity.ok().build() : ResponseEntity.notFound().build();
+        taskService.deleteTask(id);
+        return ResponseEntity.ok().build();
     }
 
 
--- src/main/java/com/nellpy/taskmanager/service/TaskService.java
+++ src/main/java/com/nellpy/taskmanager/service/TaskService.java
@@ -1,12 +1,12 @@
 package com.nellpy.taskmanager.service;
 
 import com.nellpy.taskmanager.entity.Task;
+import com.nellpy.taskmanager.exception.TaskNotFoundException;
 import com.nellpy.taskmanager.repository.TaskRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 
 @Service
@@ -21,12 +21,10 @@ public class TaskService {
     }
 
 
-    public boolean deleteTask(Long id) {
-        return taskRepository.findById(id)
-                .map(task -> {
-                    taskRepository.delete(task);
-                    return true;
-                }).orElse(false);
+    public void deleteTask(Long id) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException(id));
+        taskRepository.delete(task);
     }
 
 
@@ -40,8 +38,9 @@ public class TaskService {
     }
 
 
-    public Optional<Task> findById(Long id) {
-        return taskRepository.findById(id);
+    public Task findById(Long id) {
+        return taskRepository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException(id));
     }
 
 
@@ -55,13 +54,12 @@ public class TaskService {
     }
 
 
-    public Optional<Task> updateTask(Long id, Task updatedTask) {
-        return taskRepository.findById(id)
-                .map(task -> {
-                    task.setTitle(updatedTask.getTitle());
-                    task.setDescription(updatedTask.getDescription());
-                    task.setCompleted(updatedTask.getCompleted());
-                    return taskRepository.save(task);
-                });
+    public Task updateTask(Long id, Task updatedTask) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException(id));
+        task.setTitle(updatedTask.getTitle());
+        task.setDescription(updatedTask.getDescription());
+        task.setCompleted(updatedTask.getCompleted());
+        return taskRepository.save(task);
     }
 }
